23871e5c64662a550b34fa8a345b57b5eeebf71b
Kemal Dağ <gkemaldag@gmail.com>
2013-09-30 10:34:26 +0300

    mysql connection is now pooled

--- libs/mysql-keyval.js
+++ libs/mysql-keyval.js
@@ -7,32 +7,29 @@
 //create a local scope 
 (function(){
 	var mysql      = require('mysql');
-	var initialized = false;
-	var connection_details = {
-		  host     : 'localhost',
-		  user     : 'io4voice_kv',
-		  password : '243243',
-		  database : 'io4voice_kv',
-		  insecureAuth : true,
-	};
-	var connection = mysql.createConnection(connection_details);
+	var pool  = mysql.createPool({
+	  host     : 'localhost',
+	  user     : 'io4voice_kv',
+	  password : '243243',
+	  database : 'io4voice_kv',
+	  insecureAuth : true,
+	});
+
+	
 	var tblname = "keyval";
 
 	//private functions
-	var getDB = function(){
-		console.log("yupyup");
-		if(initialized === false){
-			initalize();
-			initialized = true;
-		}
-		
-		return connection;
+	var getDB = function(next){
+		pool.getConnection(function(err,connection){
+			if(err){
+				console.log("ERROR ON GETTING CONNECTION FROM POOL");
+			}
+			next(connection,function(){
+				connection.release();
+			});
+		});
 	}
 
-	var initalize = function(){
-		
-		connection.connect();
-	}
 
 	function noop(){
 
@@ -58,20 +55,24 @@
 			err == noop;
 		}
 
-		var con = getDB();
-		var query = "select `value` from "+tblname+" where `key` = "+con.escape(key)+"";
-		con.query(query,function(e, rows, fields){
-			if(e){
-				err(e);
-				return;
-			}
+		getDB(function(con,release){
+			var query = "select `value` from "+tblname+" where `key` = "+con.escape(key)+"";
+			con.query(query,function(e, rows, fields){
+				if(e){
+					err(e);
+					release();
+					return;
+				}
 
-			if(rows.length === 0){
-				next();
-			}else{
-				next(getJSON(rows[0].value));	
-			}
+				if(rows.length === 0){
+					next();
+				}else{
+					next(getJSON(rows[0].value));	
+				}
+				release();
+			});
 		});
+		
 	}
 
 	function set(key,value,next,err){
@@ -86,24 +87,30 @@
 			value = JSON.stringify(value);
 		}
 
-		var con = getDB();
-
-		//first get
-		get(key,function(res){
-			if(res === undefined){
-				var query = "insert into "+tblname+" (`id`,`key`,`value`) values (NULL,"+con.escape(key)+","+con.escape(value)+")";
-			}else{
-				var query = "update "+tblname+" set `value` = "+con.escape(value)+" where `key`= "+con.escape(key);
-			}
-			con.query(query,function(e, rows, fields){
-				if(e){
-					err(e);
-					return;
+		getDB(function(con,release){
+			//first get
+			get(key,function(res){
+				
+				if(res === undefined){
+					var query = "insert into "+tblname+" (`id`,`key`,`value`) values (NULL,"+con.escape(key)+","+con.escape(value)+")";
+				}else{
+					var query = "update "+tblname+" set `value` = "+con.escape(value)+" where `key`= "+con.escape(key);
 				}
- 
-				next();
+				con.query(query,function(e, rows, fields){
+					if(e){
+						err(e);
+						release();
+						return;
+					}
+					next();
+					release();
+				});
+			},function(){
+				err();
+				release();
 			});
-		},err);
+		});
+
 
 	}
 
